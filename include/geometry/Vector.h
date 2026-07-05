a9ad73e57df670520db0d008e6a91c086c3e4170
khlkarim <karimkahili002@gmail.com>
2026-07-05 22:50:35 +0100

    add operator overloading to Vec3 and Vec4 classes

--- include/geometry/Vector.h
+++ include/geometry/Vector.h
@@ -1,10 +1,15 @@
 #pragma once
 
 struct Vec2 {
-  float x, y;
+  float x = 0, y = 0;
 
-  float length();
-  float length2();
+  Vec2() = default;
+  Vec2(float a) : x(a), y(a) {}
+  Vec2(float x, float y) : x(x), y(y) {}
+
+  float length() const;
+  float length2() const;
+  friend Vec2 normalize(const Vec2 &v);
   friend float dot(const Vec2 &v1, const Vec2 &v2);
 
   Vec2 &operator+=(const Vec2 &v);
@@ -29,11 +34,70 @@ struct Vec2 {
 };
 
 struct Vec3 {
-  float x, y, z;
+  float x = 0, y = 0, z = 0;
+
+  Vec3() = default;
+  Vec3(float a) : x(a), y(a), z(a) {}
+  Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
+
+  float length() const;
+  float length2() const;
+  friend Vec3 normalize(const Vec3 &v);
+  friend float dot(const Vec3 &v1, const Vec3 &v2);
+  friend Vec3 cross(const Vec3 &v1, const Vec3 &v2);
+
+  Vec3 &operator+=(const Vec3 &v);
+  Vec3 &operator-=(const Vec3 &v);
+  Vec3 &operator*=(const Vec3 &v);
+  Vec3 &operator/=(const Vec3 &v);
+
+  Vec3 &operator+=(const float &a);
+  Vec3 &operator-=(const float &a);
+  Vec3 &operator*=(const float &a);
+  Vec3 &operator/=(const float &a);
+
+  friend Vec3 operator+(const Vec3 &v1, const Vec3 &v2);
+  friend Vec3 operator-(const Vec3 &v1, const Vec3 &v2);
+  friend Vec3 operator*(const Vec3 &v1, const Vec3 &v2);
+  friend Vec3 operator/(const Vec3 &v1, const Vec3 &v2);
+
+  friend Vec3 operator+(const Vec3 &v, const float &a);
+  friend Vec3 operator-(const Vec3 &v, const float &a);
+  friend Vec3 operator*(const Vec3 &v, const float &a);
+  friend Vec3 operator/(const Vec3 &v, const float &a);
 };
 
 struct Vec4 {
-  float x, y, z, w;
+  float x = 0, y = 0, z = 0, w = 0;
+
+  Vec4() = default;
+  Vec4(float a) : x(a), y(a), z(a), w(a) {}
+  Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+  float length() const;
+  float length2() const;
+  friend Vec4 normalize(const Vec4 &v);
+  friend float dot(const Vec4 &v1, const Vec4 &v2);
+
+  Vec4 &operator+=(const Vec4 &v);
+  Vec4 &operator-=(const Vec4 &v);
+  Vec4 &operator*=(const Vec4 &v);
+  Vec4 &operator/=(const Vec4 &v);
+
+  Vec4 &operator+=(const float &a);
+  Vec4 &operator-=(const float &a);
+  Vec4 &operator*=(const float &a);
+  Vec4 &operator/=(const float &a);
+
+  friend Vec4 operator+(const Vec4 &v1, const Vec4 &v2);
+  friend Vec4 operator-(const Vec4 &v1, const Vec4 &v2);
+  friend Vec4 operator*(const Vec4 &v1, const Vec4 &v2);
+  friend Vec4 operator/(const Vec4 &v1, const Vec4 &v2);
+
+  friend Vec4 operator+(const Vec4 &v, const float &a);
+  friend Vec4 operator-(const Vec4 &v, const float &a);
+  friend Vec4 operator*(const Vec4 &v, const float &a);
+  friend Vec4 operator/(const Vec4 &v, const float &a);
 };
 
 using Color = Vec4;
--- src/geometry/Vector.cpp
+++ src/geometry/Vector.cpp
@@ -2,9 +2,14 @@
 
 #include "geometry/Vector.h"
 
-float Vec2::length() { return std::sqrt(length2()); }
-float Vec2::length2() { return x * x + y * y; }
-float dot(const Vec2 &v1, const Vec2 &v2) { return v1.x * v2.x + v1.y * v1.y; }
+float Vec2::length() const { return std::sqrt(length2()); }
+float Vec2::length2() const { return x * x + y * y; }
+float dot(const Vec2 &v1, const Vec2 &v2) { return v1.x * v2.x + v1.y * v2.y; }
+
+Vec2 normalize(const Vec2 &v) {
+  Vec2 u = v / v.length();
+  return u;
+}
 
 Vec2 &Vec2::operator+=(const Vec2 &v) {
   this->x += v.x;
@@ -56,56 +61,338 @@ Vec2 &Vec2::operator/=(const float &a) {
 
 Vec2 operator+(const Vec2 &v1, const Vec2 &v2) {
   return {
-      .x = v1.x + v2.x,
-      .y = v1.y + v2.y,
+      v1.x + v2.x,
+      v1.y + v2.y,
   };
 }
 
 Vec2 operator-(const Vec2 &v1, const Vec2 &v2) {
   return {
-      .x = v1.x - v2.x,
-      .y = v1.y - v2.y,
+      v1.x - v2.x,
+      v1.y - v2.y,
   };
 }
 
 Vec2 operator*(const Vec2 &v1, const Vec2 &v2) {
   return {
-      .x = v1.x * v2.x,
-      .y = v1.y * v2.y,
+      v1.x * v2.x,
+      v1.y * v2.y,
   };
 }
 
 Vec2 operator/(const Vec2 &v1, const Vec2 &v2) {
   return {
-      .x = v1.x / v2.x,
-      .y = v1.y / v2.y,
+      v1.x / v2.x,
+      v1.y / v2.y,
   };
 }
 
 Vec2 operator+(const Vec2 &v, const float &a) {
   return {
-      .x = v.x + a,
-      .y = v.y + a,
+      v.x + a,
+      v.y + a,
   };
 }
 
 Vec2 operator-(const Vec2 &v, const float &a) {
   return {
-      .x = v.x - a,
-      .y = v.y - a,
+      v.x - a,
+      v.y - a,
   };
 }
 
 Vec2 operator*(const Vec2 &v, const float &a) {
   return {
-      .x = v.x * a,
-      .y = v.y * a,
+      v.x * a,
+      v.y * a,
   };
 }
 
 Vec2 operator/(const Vec2 &v, const float &a) {
   return {
-      .x = v.x / a,
-      .y = v.y / a,
+      v.x / a,
+      v.y / a,
+  };
+}
+
+float Vec3::length() const { return std::sqrt(length2()); }
+float Vec3::length2() const { return x * x + y * y + z * z; }
+float dot(const Vec3 &v1, const Vec3 &v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+
+Vec3 cross(const Vec3 &v1, const Vec3 &v2) {
+  return {
+      v1.y * v2.z - v1.z * v2.y,
+      v1.z * v2.x - v1.x * v2.z,
+      v1.x * v2.y - v1.y * v2.x,
+  };
+}
+
+Vec3 normalize(const Vec3 &v) {
+  Vec3 u = v / v.length();
+  return u;
+}
+
+Vec3 &Vec3::operator+=(const Vec3 &v) {
+  this->x += v.x;
+  this->y += v.y;
+  this->z += v.z;
+  return *this;
+}
+
+Vec3 &Vec3::operator-=(const Vec3 &v) {
+  this->x -= v.x;
+  this->y -= v.y;
+  this->z -= v.z;
+  return *this;
+}
+
+Vec3 &Vec3::operator*=(const Vec3 &v) {
+  this->x *= v.x;
+  this->y *= v.y;
+  this->z *= v.z;
+  return *this;
+}
+
+Vec3 &Vec3::operator/=(const Vec3 &v) {
+  this->x /= v.x;
+  this->y /= v.y;
+  this->z /= v.z;
+  return *this;
+}
+
+Vec3 &Vec3::operator+=(const float &a) {
+  this->x += a;
+  this->y += a;
+  this->z += a;
+  return *this;
+}
+
+Vec3 &Vec3::operator-=(const float &a) {
+  this->x -= a;
+  this->y -= a;
+  this->z -= a;
+  return *this;
+}
+
+Vec3 &Vec3::operator*=(const float &a) {
+  this->x *= a;
+  this->y *= a;
+  this->z *= a;
+  return *this;
+}
+
+Vec3 &Vec3::operator/=(const float &a) {
+  this->x /= a;
+  this->y /= a;
+  this->z /= a;
+  return *this;
+}
+
+Vec3 operator+(const Vec3 &v1, const Vec3 &v2) {
+  return {
+      v1.x + v2.x,
+      v1.y + v2.y,
+      v1.z + v2.z,
+  };
+}
+
+Vec3 operator-(const Vec3 &v1, const Vec3 &v2) {
+  return {
+      v1.x - v2.x,
+      v1.y - v2.y,
+      v1.z - v2.z,
+  };
+}
+
+Vec3 operator*(const Vec3 &v1, const Vec3 &v2) {
+  return {
+      v1.x * v2.x,
+      v1.y * v2.y,
+      v1.z * v2.z,
+  };
+}
+
+Vec3 operator/(const Vec3 &v1, const Vec3 &v2) {
+  return {
+      v1.x / v2.x,
+      v1.y / v2.y,
+      v1.z / v2.z,
+  };
+}
+
+Vec3 operator+(const Vec3 &v, const float &a) {
+  return {
+      v.x + a,
+      v.y + a,
+      v.z + a,
+  };
+}
+
+Vec3 operator-(const Vec3 &v, const float &a) {
+  return {
+      v.x - a,
+      v.y - a,
+      v.z - a,
+  };
+}
+
+Vec3 operator*(const Vec3 &v, const float &a) {
+  return {
+      v.x * a,
+      v.y * a,
+      v.z * a,
+  };
+}
+
+Vec3 operator/(const Vec3 &v, const float &a) {
+  return {
+      v.x / a,
+      v.y / a,
+      v.z / a,
+  };
+}
+
+float Vec4::length() const { return std::sqrt(length2()); }
+float Vec4::length2() const { return x * x + y * y + z * z + w * w; }
+float dot(const Vec4 &v1, const Vec4 &v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w; }
+
+Vec4 normalize(const Vec4 &v) {
+  Vec4 u = v / v.length();
+  return u;
+}
+
+Vec4 &Vec4::operator+=(const Vec4 &v) {
+  this->x += v.x;
+  this->y += v.y;
+  this->z += v.z;
+  this->w += v.w;
+  return *this;
+}
+
+Vec4 &Vec4::operator-=(const Vec4 &v) {
+  this->x -= v.x;
+  this->y -= v.y;
+  this->z -= v.z;
+  this->w -= v.w;
+  return *this;
+}
+
+Vec4 &Vec4::operator*=(const Vec4 &v) {
+  this->x *= v.x;
+  this->y *= v.y;
+  this->z *= v.z;
+  this->w *= v.w;
+  return *this;
+}
+
+Vec4 &Vec4::operator/=(const Vec4 &v) {
+  this->x /= v.x;
+  this->y /= v.y;
+  this->z /= v.z;
+  this->w /= v.w;
+  return *this;
+}
+
+Vec4 &Vec4::operator+=(const float &a) {
+  this->x += a;
+  this->y += a;
+  this->z += a;
+  this->w += a;
+  return *this;
+}
+
+Vec4 &Vec4::operator-=(const float &a) {
+  this->x -= a;
+  this->y -= a;
+  this->z -= a;
+  this->w -= a;
+  return *this;
+}
+
+Vec4 &Vec4::operator*=(const float &a) {
+  this->x *= a;
+  this->y *= a;
+  this->z *= a;
+  this->w *= a;
+  return *this;
+}
+
+Vec4 &Vec4::operator/=(const float &a) {
+  this->x /= a;
+  this->y /= a;
+  this->z /= a;
+  this->w /= a;
+  return *this;
+}
+
+Vec4 operator+(const Vec4 &v1, const Vec4 &v2) {
+  return {
+      v1.x + v2.x,
+      v1.y + v2.y,
+      v1.z + v2.z,
+      v1.w + v2.w,
+  };
+}
+
+Vec4 operator-(const Vec4 &v1, const Vec4 &v2) {
+  return {
+      v1.x - v2.x,
+      v1.y - v2.y,
+      v1.z - v2.z,
+      v1.w - v2.w,
+  };
+}
+
+Vec4 operator*(const Vec4 &v1, const Vec4 &v2) {
+  return {
+      v1.x * v2.x,
+      v1.y * v2.y,
+      v1.z * v2.z,
+      v1.w * v2.w,
+  };
+}
+
+Vec4 operator/(const Vec4 &v1, const Vec4 &v2) {
+  return {
+      v1.x / v2.x,
+      v1.y / v2.y,
+      v1.z / v2.z,
+      v1.w / v2.w,
+  };
+}
+
+Vec4 operator+(const Vec4 &v, const float &a) {
+  return {
+      v.x + a,
+      v.y + a,
+      v.z + a,
+      v.w + a,
+  };
+}
+
+Vec4 operator-(const Vec4 &v, const float &a) {
+  return {
+      v.x - a,
+      v.y - a,
+      v.z - a,
+      v.w - a,
+  };
+}
+
+Vec4 operator*(const Vec4 &v, const float &a) {
+  return {
+      v.x * a,
+      v.y * a,
+      v.z * a,
+      v.w * a,
+  };
+}
+
+Vec4 operator/(const Vec4 &v, const float &a) {
+  return {
+      v.x / a,
+      v.y / a,
+      v.z / a,
+      v.w / a,
   };
 }
